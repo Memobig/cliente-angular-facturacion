6ba0ecb5efa01130dd9d0e9e71c09b9a44d6457c
Memobig <memobig@gmail.com>
2018-08-30 11:28:34 -0500

    cliente angular CRUD

---
+++ src/app/clientes/cliente.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CLIENTES } from './clientes.json';
+import { Cliente } from './cliente';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  private urlEndPoint: string = 'http://189.133.176.129:8080/api/clientes';
+
+  private httpHeaders  = new HttpHeaders({'Content-Type':'application/json'});
+
+  constructor(private http: HttpClient) { }
+
+  getClientes(): Observable<Cliente[]> {
+    //return of(CLIENTES);
+
+  //  return this.http.get(this.urlEndPoint).pipe(
+  //    map(response => response as Cliente[])
+  //  );
+
+    return this.http.get<Cliente[]>(this.urlEndPoint);
+  }
+
+  create(cliente: Cliente):Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders})
+  }
+}
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id:1, nombre:'Guillermo', apellido:'Ramirez', email:'guiram@gmail.com', createAt:'2017-04-20'},
+  {id:2, nombre:'Pablo', apellido:'Lopez', email:'qqqq@gmail.com', createAt:'2007-04-20'},
+  {id:3, nombre:'Hector', apellido:'Garcia', email:'aaaaa@gmail.com', createAt:'2011-04-20'},
+  {id:4, nombre:'Mario', apellido:'Osorio', email:'fffff@gmail.com', createAt:'2012-04-20'},
+  {id:5, nombre:'Juan', apellido:'Ramirez', email:'bbbbb@gmail.com', createAt:'2018-04-20'}
+];
