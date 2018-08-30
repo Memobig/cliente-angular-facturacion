1cfc468b96f9ac23ba6cfbcaed83a7ac1b474442
Memobig <memobig@gmail.com>
2018-08-30 12:58:24 -0500

    se agrego botón editar y se mapeo con el formulario

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,32 +1,37 @@
-import { Injectable } from '@angular/core';
-import { CLIENTES } from './clientes.json';
-import { Cliente } from './cliente';
-import { Observable, of } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { CLIENTES } from "./clientes.json";
+import { Cliente } from "./cliente";
+import { Observable, of } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ClienteService {
+  private urlEndPoint: string = "http://localhost:8080/api/clientes";
 
-  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  private httpHeaders = new HttpHeaders({ "Content-Type": "application/json" });
 
-  private httpHeaders  = new HttpHeaders({'Content-Type':'application/json'});
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getClientes(): Observable<Cliente[]> {
     //return of(CLIENTES);
 
-  //  return this.http.get(this.urlEndPoint).pipe(
-  //    map(response => response as Cliente[])
-  //  );
+    //  return this.http.get(this.urlEndPoint).pipe(
+    //    map(response => response as Cliente[])
+    //  );
 
     return this.http.get<Cliente[]>(this.urlEndPoint);
   }
 
-  create(cliente: Cliente):Observable<Cliente> {
-    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders})
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {
+      headers: this.httpHeaders
+    });
+  }
+
+  getCliente(id): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`);
   }
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,33 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { Cliente } from './cliente';
-import { ClienteService } from './cliente.service';
-import { Router } from '@angular/router';
-import  swal  from 'sweetalert2';
+import { Component, OnInit } from "@angular/core";
+import { Cliente } from "./cliente";
+import { ClienteService } from "./cliente.service";
+import { Router, ActivatedRoute } from "@angular/router";
+import swal from "sweetalert2";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html'
+  selector: "app-form",
+  templateUrl: "./form.component.html"
 })
 export class FormComponent implements OnInit {
   private cliente: Cliente = new Cliente();
 
-  private titulo: string = "Crear Cliente"
+  private titulo: string = "Crear Cliente";
 
-  constructor(private clienteService: ClienteService,
-  private router: Router) {}
+  constructor(
+    private clienteService: ClienteService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.cargarCliente();
   }
 
-  public create(): void{
+  cargarCliente(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params["id"];
+      if (id) {
+        this.clienteService
+          .getCliente(id)
+          .subscribe(cliente => (this.cliente = cliente));
+      }
+    });
+  }
+
+  public create(): void {
     console.log("Clicked!");
     console.log(this.cliente);
-    this.clienteService.create(this.cliente)
-    .subscribe( cliente => {
-      this.router.navigate(['/clientes'])
-      swal('Nuevo cliente', `Cliente ${cliente.nombre} creado con éxito!`, 'success');
-    }
-    );
+    this.clienteService.create(this.cliente).subscribe(cliente => {
+      this.router.navigate(["/clientes"]);
+      swal(
+        "Nuevo cliente",
+        `Cliente ${cliente.nombre} creado con éxito!`,
+        "success"
+      );
+    });
   }
-
 }
